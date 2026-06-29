39e0914aa8c63a9e8df5af7bf8732939f66beb13
NguyenNgocBich <nguyenngocbich181106@gmail.com>
2026-06-29 20:13:04 +0700

    lam not tại vì file này trong 1 repo khác

--- baitaptuan6.cpp
+++ baitaptuan6.cpp
@@ -37,4 +37,53 @@ void insertSorted(Node*& head, File f) {
 
     node->next = cur->next;
     cur->next = node;
+}//hien thi danh sach
+void display(Node* head) {
+    while (head != NULL) {
+        cout << head->data.name << " | "
+             << head->data.size << "MB | "
+             << head->data.time << endl;
+        head = head->next;
+    }
+}
+//tinh tong dung luong
+float totalSize(Node* head) {
+    float sum = 0;
+    while (head != NULL) {
+        sum += head->data.size;
+        head = head->next;
+    }
+    return sum;
+}
+
+// Xóa file nhỏ nhất
+void removeSmallest(Node*& head) {
+    if (head == NULL) return;
+
+    Node *minNode = head, *minPrev = NULL;
+    Node *cur = head, *prev = NULL;
+
+    while (cur != NULL) {
+        if (cur->data.size < minNode->data.size) {
+            minNode = cur;
+            minPrev = prev;
+        }
+        prev = cur;
+        cur = cur->next;
+    }
+
+    if (minPrev == NULL) {
+        head = head->next;
+    } else {
+        minPrev->next = minNode->next;
+    }
+
+    delete minNode;
+}
+
+// Fit vào USB
+void fitToUSB(Node*& head, float limit) {
+    while (totalSize(head) > limit) {
+        removeSmallest(head);
+    }
 }
